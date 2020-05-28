a788b1656ca83bb368f1db72afef6467b2c70edd
Inkagnedo <CCelestin44@gmail.com>
2020-05-28 18:59:32 -0400

    getting started

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Home } from './Home';
+import { Login } from './Login';
+import { Signup } from './Signup';
+import { Main } from './Main';
+import { NoMatch } from './NoMatch';
+import { Layout } from './components/Layout';
+import { NavBar } from './components/NavBar';
+import { Jumbotron } from './components/Jumbotron'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <Jumbotron />
+        <Layout>
+          <Router>
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route path='/login' component={Login} />
+              <Route path='/signup' component={Signup} />
+              <Route path='/main' component={Main} />
+              <Route component={NoMatch} />
+            </Switch>
+          </Router>
+        </Layout>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Jumbotron.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Jumbotron as Jumbo, Container } from 'react-bootstrap';
+import styled from 'styled-components';
+import spBannerImage from '../assets/superGoatBanner.jpg';
+
+const Styles = styled.div`
+  .jumbo {
+    background: url(${spBannerImage}) no-repeat fixed bottom;
+    background-size: cover;
+    color: #0C0608;
+    height: 300px;
+    position: relative;
+    z-index: -2;
+  }
+  .overlay {
+    background-color: #000;
+    opacity: 0;
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    z-index: -1;
+  }
+`;
+
+export const Jumbotron = () => (
+  <Styles>
+    <Jumbo fluid className="jumbo">
+      <div className="overlay"></div>
+      <Container>
+        <p>The Ultimate Sports Stat Tracker</p>
+      </Container>
+    </Jumbo>
+  </Styles>
+)
